474886242fd8badf6665aa94aef7026777e5aaeb
Dhansukh <81100455+Dhansukh1206@users.noreply.github.com>
2024-08-02 23:01:42 +0530

    url change for be

--- routes/video.js
+++ routes/video.js
@@ -18,9 +18,10 @@ router.get("/", (req, res) => {
       return res.status(500).send("Unable to scan files.");
     }
     const videoFiles = files.filter((file) => /\.(mp4|mkv|avi)$/i.test(file));
+    console.log('videoFiles', videoFiles)
     res.status(200).json(
       videoFiles.map((file) => ({
-        filePath: `http://localhost:8080/uploads/${file}`,
+        filePath: `https://desolate-eyrie-13966-6cda0935eea4.herokuapp.com/uploads/${file}`,
       }))
     );
   });
